2638ac93ed418f6ea063c805c8aad8f1db6b3317
Popgeorgiev <georgi.popgeorgiev@student.vinci.be>
2025-03-14 11:06:50 +0100

    add : method in class Graph

---
+++ Graph.java
@@ -0,0 +1,13 @@
+public class Graph {
+
+
+    public String trouverCheminLePlusCourt (String artiste, String artiste2){
+        return null;
+    }
+
+    public String trouverCheminMaxMention (String artiste, String artiste2){
+        return null;
+    }
+
+
+}
