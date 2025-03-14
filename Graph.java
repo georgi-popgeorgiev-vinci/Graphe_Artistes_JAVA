426270dc540eb10d696f6194d0b619c0c90d8806
kyllian-delbauche-vinci <kylliandelbauchep@gmail.com>
2025-03-14 11:16:35 +0100

    add constructor

--- Graph.java
+++ Graph.java
@@ -1,5 +1,12 @@
 public class Graph {
 
+    private String artistsFile;
+    private String mentionsFile;
+
+    public Graph(String artistsFile, String mentionsFile) {
+        this.artistsFile = artistsFile;
+        this.mentionsFile = mentionsFile;
+    }
 
     public String trouverCheminLePlusCourt (String artiste, String artiste2){
         return null;
